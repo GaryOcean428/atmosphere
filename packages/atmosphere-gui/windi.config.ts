75c811fd3ca2ad4ef7fc021ff00dc9051cc1795b
GaryOcean <81794144+GaryOcean428@users.noreply.github.com>
2026-08-19 14:07:36 +0000

    fix(gui): reorder WindiCSS plugin imports for consistency
    Signed-off-by: GitHub <noreply@github.com>

--- packages/atmosphere-gui/windi.config.ts
+++ packages/atmosphere-gui/windi.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'windicss/helpers'
-import formsPlugin from 'windicss/plugin/forms'
-import typographyPlugin from 'windicss/plugin/typography'
-import aspectRatioPlugin from 'windicss/plugin/aspect-ratio'
-import lineClampPlugin from 'windicss/plugin/line-clamp'
 import windiColors from 'windicss/colors'
+import { defineConfig } from 'windicss/helpers'
+import aspectRatioPlugin from 'windicss/plugin/aspect-ratio'
+import formsPlugin from 'windicss/plugin/forms'
+import lineClampPlugin from 'windicss/plugin/line-clamp'
+import typographyPlugin from 'windicss/plugin/typography'
 // @ts-expect-error no types for plugin-scrollbar
 import scrollbar from '@windicss/plugin-scrollbar'
 // @ts-expect-error no types for plugin-animation
